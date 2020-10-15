1c0ec0a2ac3701413d94384afa27057dba5bb6bb
unknown <rune.escape888@gmail.com>
2020-10-15 13:03:59 -0400

    ADD: Uplad images

---
+++ backend/controllers/images.js
@@ -0,0 +1,38 @@
+
+
+export const uploadImage = async (req, res) => {
+	if (!req.files) {
+		return res.status(500).json({
+			status: false, 
+			message: 'Not File Found',
+		});
+	}
+	const { name, data, size, mimetype, mv } = req.files.image;
+
+	if (!imageMimeType.includes(mimetype)){
+		return res.status(500).json({
+			status: false, 
+			message: 'Invalid File Type',
+		});
+	}
+
+	const imageUrl = `data:${mimetype};charset=utf-8;base64,${data.toString('base64')}`;
+	const image = new Image({ 
+		name: name + Date.now(),
+		imageUrl, 
+		size, 
+		mimetype 
+	});
+
+	try{
+		const savedImage = await image.save(); 
+		res.json({
+			status: true,
+			message: 'File Uploaded',
+			imageUrl,
+		});
+	}
+	catch(err){
+		res.status(500).json({ message: err.message});
+	}
+}
