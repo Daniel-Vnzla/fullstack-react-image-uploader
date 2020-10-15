9d6704658dc99dee6343dc55694d824f28c22dcb
unknown <rune.escape888@gmail.com>
2020-10-15 10:26:22 -0400

    ADD file upload dependency

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dovenv = require('dotenv').config();
+const fileUpload = require('express-fileupload');
 const app = express();
 
 // Data base
@@ -13,6 +14,9 @@ const PORT = process.env.PORT || 3000;
 // Settings
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
+//Routes
+app.use('/',require('./routes/images.js'));
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
