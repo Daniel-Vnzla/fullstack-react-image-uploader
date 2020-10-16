0f2743dd1dc263789b5e5814401f9ca8e520a06d
unknown <rune.escape888@gmail.com>
2020-10-16 17:35:08 -0400

    add: gallery components

---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+	
+</script>
+
+<header class="header">
+	<button class="upload-btn">Upload</button>
+	<h1 class="title">Mob</h1>
+</header>
+
+
+
+<style>
+	.header {
+		display: flex;
+		max-width: 90%;
+		justify-content: flex-end;
+		align-items: flex-end;
+		height: 80px;
+	}
+
+	.title {
+		font-size: 3rem;
+		line-height: 2.8rem;
+		color: #fff;
+	}
+
+	.upload-btn {
+		background: linear-gradient(to left, #5F40C8, #CB50CF, #8E1F8E);
+		border: none;
+		color: #fff;
+		padding: 9px 40px;
+		margin-right: 20px;
+		border-radius: 6px;
+		font-weight: bold;
+		cursor: pointer;
+	}
+</style>
