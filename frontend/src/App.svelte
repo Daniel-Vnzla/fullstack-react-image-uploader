ded310813da510837fd96fe76459c9a3ad090199
unknown <rune.escape888@gmail.com>
2020-10-16 17:04:47 -0400

    initialize app

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import DragAndDrop from './components/DragAndDrop.svelte';
+</script>
+
+<main>
+	<DragAndDrop />
+</main>
+
+<style>
+	main {
+		max-width: 1000px;
+		margin: 0 auto;
+	}
+</style>
